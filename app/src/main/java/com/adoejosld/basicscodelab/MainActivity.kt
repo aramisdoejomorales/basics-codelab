cd7286993aed893156cf70d07ae8c81fd2526698
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-03-21 23:00:33 -0400

    add .

---
+++ app/src/main/java/com/adoejosld/basicscodelab/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.adoejosld.basicscodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.adoejosld.basicscodelab.ui.theme.BasicsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                MyApp(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("Android", "Kotlin", "Jetpack Compose")
+) {
+    Column(
+        modifier = modifier
+            .padding(vertical = 4.dp)
+    ) {
+        for (name in names)
+            Greeting(name = name)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(horizontal = 4.dp, vertical = 4.dp)
+    ) {
+        Column(modifier = modifier
+            .fillMaxWidth()
+            .padding(12.dp)) {
+            Text(
+                text = "Hello...", color = Color.Black
+            )
+            Text(
+                text = "$name!", color = Color.Red
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingPreview() {
+    BasicsCodelabTheme {
+        MyApp()
+    }
+}
