1596cb6830aa332915f9796397c40ab6937b8e6d
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-03-26 20:06:22 -0400

    add .

--- app/src/main/java/com/adoejosld/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/adoejosld/basicscodelab/MainActivity.kt
@@ -3,17 +3,22 @@ package com.adoejosld.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,7 +40,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("Android", "Kotlin", "Jetpack Compose")
+    names: List<String> = listOf(
+        "Android",
+        "Kotlin",
+        "Jetpack Compose",
+        "by Android Studio",
+        "Aramis Doejo, Médico General / Ecografísta"
+    )
 ) {
     Column(
         modifier = modifier
@@ -73,6 +84,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 Text(if (expanded.value) "Mostrar -" else "Mostrar +")
             }
         }
+    }
+}
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Bienvenido a Basics Codelab")
+        Button(
+            modifier = modifier.padding(vertical = 24.dp),
+            onClick = { shouldShowOnboarding = false }) {
+            Text("Continuar")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun ShouldShowOnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen()
     }
 }
 
