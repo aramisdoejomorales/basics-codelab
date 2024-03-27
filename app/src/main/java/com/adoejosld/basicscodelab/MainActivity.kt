b38a600a8a23b799cce6785ab2b75e3aed492ebb
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-03-27 00:39:00 -0400

    add .

--- app/src/main/java/com/adoejosld/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/adoejosld/basicscodelab/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.adoejosld.basicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,11 +22,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.adoejosld.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -41,7 +46,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
     Surface(modifier) {
         if (shouldShowOnboarding) {
             OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
@@ -53,12 +58,10 @@ fun MyApp(modifier: Modifier = Modifier) {
 
 @Composable
 private fun Greetings(
-    modifier: Modifier = Modifier,
-    names: List<String> = List(100) { "$it" }
+    modifier: Modifier = Modifier, names: List<String> = List(100) { "$it" }
 ) {
     LazyColumn(
-        modifier = modifier
-            .padding(vertical = 4.dp)
+        modifier = modifier.padding(vertical = 4.dp)
     ) {
         items(items = names) { name -> Greeting(name = name) }
 
@@ -67,23 +70,29 @@ private fun Greetings(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var expanded by remember { mutableStateOf(false) }
-    val extraPadding = if (expanded) 60.dp else 0.dp
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp, label = "Aro", animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow
+        )
+    )
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.padding(vertical = 2.dp, horizontal = 4.dp)
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(12.dp)) {
+        Row(modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
+                Text(text = "Hello...")
                 Text(
-                    text = "Hello...", color = Color.Black
-                )
-                Text(
-                    text = name, color = Color.Red
+                    text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ), color = Color.Red
                 )
             }
             ElevatedButton(onClick = { expanded = !expanded }) {
@@ -102,8 +111,7 @@ fun OnboardingScreen(onContinueClicked: () -> Unit, modifier: Modifier = Modifie
     ) {
         Text("Bienvenido a Basics Codelab")
         Button(
-            modifier = modifier.padding(vertical = 24.dp),
-            onClick = onContinueClicked
+            modifier = modifier.padding(vertical = 24.dp), onClick = onContinueClicked
         ) {
             Text("Continuar")
         }
@@ -127,6 +135,10 @@ fun MyAppPreview() {
     }
 }
 
+@Preview(showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDark")
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingsPreview() {
